4753f5dd1881be7a2775bd6189d30208b67da1ee
Julio Cesar Foronda Giraldo <notyvarbaro@gmail.com>
2025-07-23 09:07:35 -0500

    Primer commit utilizando java script

---
+++ script.js
@@ -0,0 +1,9 @@
+/* console.log("Esto es un mensaje de prueba desde script.js");
+console.log('Mensaje con comillas simples');
+console.log(25 + 25);
+console.log(true);
+console.log(34 > 5);
+alert("Te hare una pregunta");
+prompt("¿Cual es tu nombre?");
+console.log("Hola, " + prompt("¿Cual es tu nombre?") + " Bienvenido a mi sitio web.");
+ */
